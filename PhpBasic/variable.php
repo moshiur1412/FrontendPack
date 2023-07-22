9c890119e0df7c7814b84f1615eb97ad91c941d3
moshiur1412 <moshiur1412@gmail.com>
2023-07-22 10:58:40 +0600

    PHP & JS implemented for Course module

---
+++ PhpBasic/variable.php
@@ -0,0 +1,42 @@
+<?php
+# 1. String Type 
+// $name = "Abdur Rahim";
+// $nameValue = 'Abdur Rahim 2nd Value';
+# Output Value -->
+// echo $name;
+// print $name;
+// print_r($name);
+// var_dump($name);
+
+// echo gettype($name);
+// print $nameValue;
+
+
+
+
+# 2. Integer
+// $intValue = 10;
+// echo $intValue . "\n"; // 10
+// print(gettype($intValue) . "\n"); // integer
+// var_dump($intValue); // int(10)
+
+
+// Float
+$floatValue = 10.5;
+// var_dump($floatValue);
+
+// Boolean
+$bol = true;
+var_dump($bol);
+
+// Array
+
+// Object
+
+// Resources
+// Callable
+
+// phpinfo();
+
+// echo "Alhamduillah, we are staring php language";
+
--- js/rawjs.js
+++ js/rawjs.js
@@ -1,91 +1,122 @@
-// Variables -->
-var name = "Abdur Rohim"; // String
-var age = 20; // Intiger 
-var children = ["M", "N", "O", "P"]; // Array [0]
-var car = null; // Null
-var salary = 20.50; // Float 
-var obj = { 'name': "m", age : 20, car : 1 } // Object (Json)
+// Data Type of Variables -->
+/* 1. Srting */ 
+// var humanName = "Abdur Rohim"; // String
+// var carName = "Volvo";
+// carName = "BMW"; // Value Values --> 
+// let valueTest = `Single \\ code \\ value`; // Back-Tics Syntax 
+// let valueTest2 = "Single \\code\\ value"; // Escape Backslash Character
 
-// console.log(obj.car);
+// JS Output --->
+// console.log(humanName); // Developer => Debugs/Errors
+// window.alert(humanName);
+// alert(humanName);
+// document.write(humanName);
+// docuement.getElementById('html_tag_element_id_Name').innerHtML(humanName);
+// console.log({humanhumanName});
+// console.log(typeof carName);
+// console.log(valueTest2);
+// console.log("Abour Korim"); // Fixed Value --> literals 
 
-
-
-// Variable Declear type name --> 
-// var = > 2015 // Reassign + ReDeclear 
-// let = 2015 > 
-// const = 2015 > 
-
-// Reassign + ReDeclear --> 
-var value = "Your first name here...";
-var value = "Enter you second name...";
-
-// ReDeclear --> 
-// let value = "plz enter let value"; // SyntaxError: Identifier 'value' has already been declared
-let let_value = "plz enter let value";
-// let let_value = "plz enter 2nd let value";  // SyntaxError: Identifier 'let_value' has already been declared
-let_value = "plz enter 2nd let value"; // 
+/* 2. Integer */ 
+var intValue = 1011111111111;
+console.log(intValue); // 10 
+// console.log(typeof intValue); // Number 15
+console.log(intValue.toString().length);
 
 
-// NOT Redecler Or Reassign 
-// const value = "plz enter const value"; // SyntaxError: Identifier 'value' has already been declared
-const constValue = "plz enter const value";
-// const constValue = "plz enter const value"; // Identifier 'constValue' has already been declared
-// constValue = "plz enter 2nd const value"; // TypeError: Assignment to constant variable
+/* 3. float */
+var floatValue = 10.444;
+// console.log(floatValue);
+// console.log(typeof floatValue);
+// console.log(Number.isFinite(floatValue));
+
+
+// var age = 20; // Intiger 
+// var children = ["M", "N", "O", "P"]; // Array [0]
+// var car = null; // Null
+// var salary = 20.50; // Float 
+// var obj = { 'name': "m", age : 20, car : 1 } // Object (Json)
+
+// // console.log(obj.car);
 
 
 
-var num1 = 25;
-var num2 = 55;
+// // Variable Declear type name --> 
+// // var = > 2015 // Reassign + ReDeclear 
+// // let = 2015 > 
+// // const = 2015 > 
+
+// // Reassign + ReDeclear --> 
+// var value = "Your first name here...";
+// var value = "Enter you second name...";
+
+// // ReDeclear --> 
+// // let value = "plz enter let value"; // SyntaxError: Identifier 'value' has already been declared
+// let let_value = "plz enter let value";
+// // let let_value = "plz enter 2nd let value";  // SyntaxError: Identifier 'let_value' has already been declared
+// let_value = "plz enter 2nd let value"; // 
 
 
-// var number = [1,2,3,4,5,6];
+// // NOT Redecler Or Reassign 
+// // const value = "plz enter const value"; // SyntaxError: Identifier 'value' has already been declared
+// const constValue = "plz enter const value";
+// // const constValue = "plz enter const value"; // Identifier 'constValue' has already been declared
+// // constValue = "plz enter 2nd const value"; // TypeError: Assignment to constant variable
 
-for(var i=0; i<=100; i++){
-    // var array = i; // can print outside of the blog
-    // const value_from_loop = [i]; // const not print outside of the blog
 
 
-    if (i >= 0 && i <= 32){
-        console.log("failed: " + i);
-    }
+// var num1 = 25;
+// var num2 = 55;
 
-    else if (i >= 33 && i < 50){
-        console.log("Passed: " + i);
-    }
-    else if (i >= 50 && i < 70){
-        console.log("Average: " + i);
-    }
-    else if (i >= 70 && i < 80){
-        console.log("Good Student: " + i);
-    }
-    // else if (i >= 80 && i < 100){
-    //     console.log("Great Student: " + i);
-    // }
 
-    else{
-        console.log("Great Student: " + i);
-    }
-}
-    // else if (i < 33 && i > 49){
-    //     console.log("Passed : "+i);
-    // }
-    // else if (i < 49 && i > 79){
-    //     console.log("Average : "+i);
-    // }
-    // else if (i < 79 && i > 100){
-    //     console.log("Good Student: "+i);
-    // }
-    // else{
-    //     console.log("Gread student: "+i);
-    // }
+// // var number = [1,2,3,4,5,6];
+
+// for(var i=0; i<=100; i++){
+//     // var array = i; // can print outside of the blog
+//     // const value_from_loop = [i]; // const not print outside of the blog
+
+
+//     if (i >= 0 && i <= 32){
+//         console.log("failed: " + i);
+//     }
+
+//     else if (i >= 33 && i < 50){
+//         console.log("Passed: " + i);
+//     }
+//     else if (i >= 50 && i < 70){
+//         console.log("Average: " + i);
+//     }
+//     else if (i >= 70 && i < 80){
+//         console.log("Good Student: " + i);
+//     }
+//     // else if (i >= 80 && i < 100){
+//     //     console.log("Great Student: " + i);
+//     // }
+
+//     else{
+//         console.log("Great Student: " + i);
+//     }
+// }
+//     // else if (i < 33 && i > 49){
+//     //     console.log("Passed : "+i);
+//     // }
+//     // else if (i < 49 && i > 79){
+//     //     console.log("Average : "+i);
+//     // }
+//     // else if (i < 79 && i > 100){
+//     //     console.log("Good Student: "+i);
+//     // }
+//     // else{
+//     //     console.log("Gread student: "+i);
+//     // }
         
      
 
 
 
 
-function calculator(...$arg){
-    return $arg;
-}
-console.log("=====================================================================================");
-// console.log(value_from_loop);
+// function calculator(...$arg){
+//     return $arg;
+// }
+// console.log("=====================================================================================");
+// // console.log(value_from_loop);
